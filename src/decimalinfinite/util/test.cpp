6d27b9b7b26293b1f68673e98fb4cc20159f7931
Ghislain Fourny <ghislain.fourny@gmail.com>
2017-02-01 14:30:26 +0100

    Add files.

---
+++ src/decimalinfinite/util/test.cpp
@@ -0,0 +1,39 @@
+#include "test.h"
+
+#include <iostream>
+
+bool Test::assertBoolEqual(bool expected, bool actual) {
+	if (actual == expected) {
+		std::cout << "[PASSED]" << std::endl;
+		return true;
+	} else {
+		std::cout << "[FAIL]" << std::endl;
+		std::cout << "Actual:" << actual << std::endl;
+		std::cout << "Expected: " << expected << std::endl;
+		return false;
+	}
+}
+
+bool Test::assertIntEqual(int expected, int actual) {
+	if (actual == expected) {
+		std::cout << "[PASSED]" << std::endl;
+		return true;
+	} else {
+		std::cout << "[FAIL]" << std::endl;
+		std::cout << "Actual:" << actual << std::endl;
+		std::cout << "Expected: " << expected << std::endl;
+		return false;
+	}
+}
+
+bool Test::assertStringEqual(std::string expected, std::string actual) {
+	if (actual == expected) {
+		std::cout << "[PASSED]" << std::endl;
+		return true;
+	} else {
+		std::cout << "[FAIL]" << std::endl;
+		std::cout << "Actual:" << actual << std::endl;
+		std::cout << "Expected: " << expected << std::endl;
+		return false;
+	}
+}
---
+++ src/decimalinfinite/util/test.h
@@ -0,0 +1,8 @@
+#import <string>
+
+class Test {
+public:
+	static bool assertBoolEqual(bool, bool);
+  static bool assertStringEqual(std::string, std::string);
+  static bool assertIntEqual(int, int);
+};
